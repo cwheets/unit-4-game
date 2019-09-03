269ac9a56ec72c966d5f2c838be32d881d502409
wheeters <wheetmansw@gmail.com>
2019-09-02 19:54:18 -0700

    added html and intial js

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+$( document ).ready(function (){
+var losses = $("#lose-text")
+var wins = $("#wins-text");
+
+
+crystalGen1 = 
+Math.floor(Math.random()* 20)
+crystalGen2 = 
+Math.floor(Math.random()* 20)
+crystalGen3 = 
+Math.floor(Math.random()* 20)
+crystalGen4 = 
+Math.floor(Math.random()* 20)
+
+var crystal1 = crystalGen1;
+var crystal2 = crystalGen2; 
+var crystal3 = crystalGen3; 
+var crystal4 = crystalGen4; 
+
+crystal1 = $("#crystal1")
+crystal2 = $("#crystal1")
+crystal3 = $("#crystal1")
+crystal4 = $("#crystal1")
+
+console.log(crystal1, crystal2)
+
+
+})
