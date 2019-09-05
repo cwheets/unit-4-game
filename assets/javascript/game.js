36478528749220abb14fea26857eaff303a44396
wheeters <wheetmansw@gmail.com>
2019-09-04 19:28:10 -0700

    refined click functions as well as reset and winCon functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,35 @@
-$( document ).ready(function (){
+$(document).ready(function () {
+    // var state = {
+    //     elements: {
+    //         lossesText: $()
+    //     },
+    //     wins: 0,
+    //     losses
+    // }
 var lossesText = $("#lose-text")
 var winsText = $("#wins-text");
-var crystals = ['crystal1','crystal2','crystal3','crystal4']
+var crystals = []
 var computerStartingNumber = $("#comp-score")
 var userTotalid = $("#user-total")
-var usertotal = ''
+var userTotal = 0
+var startingNumber;
+var winsInt = 0
+var lossesInt = 0
+var crystalElements = [ $("#crystal1"), $("#crystal2"), $("#crystal3"), $("#crystal4") ]
 
-startingNumberGen = function(){
+var startingNumberGen = function (){
     var randomStartingNumber = (Math.floor(Math.random()* 70)+50)
     return(randomStartingNumber)
 }
 
-fillCrystalArray = function(){
+var fillCrystalArray = function(){
 numbergenC = function(){
 var randomNumberC = (Math.floor(Math.random()* 20)+1)
 return (randomNumberC)
 }
 for (var i = 0; i < crystals.length; i++) {
-     crystals[i] = numbergenC()
+     crystals.push(numbergenC())
+     crystalElements[i].attr("crystal-index", i);
 }   
 
 }
@@ -25,28 +37,43 @@ for (var i = 0; i < crystals.length; i++) {
 fillCrystalArray()
 startingNumberGen()
 computerStartingNumber.text(startingNumberGen())
-
- var crystal1 = $("#crystal1")
- var crystal2 = $("#crystal2")
- var crystal3 = $("#crystal3")
- var crystal4 = $("#crystal4")
-console.log(crystals[0])
+startingNumber = startingNumberGen()
 
 
-crystal1.click( function() {
-    crystals[0] + usertotal
+
+
+
+var gem = $('.gem')
+
+gem.click(function(event) {
+    var index = parseInt($(this).attr("crystal-index"))
+    userTotal += crystals[index]
+    userTotalid.text(userTotal)
+    winCon()
+
  })
- crystal2.click( function() {
-    crystals[1] =+ usertotal
-})
-crystal3.click( function() {
-    crystals[2] =+ usertotal
-})
-crystal4.click( function() {
-    crystals[3] =+ usertotal
-})
 
-console.log(usertotal)
+function winCon (){
+    if (userTotal===startingNumber){
+        console.log("you won")
+        winsInt++
+        winsText.text(winsInt)
+        reset()
+    }
+    else if(userTotal > startingNumber){
+        console.log('you lost')
+        lossesInt ++
+        lossesText.text(lossesInt)
+        reset()
+    }
+}
+function reset(){
+    fillCrystalArray()
+    startingNumberGen()
+    computerStartingNumber.text(startingNumberGen())
 
+}
 
+console.log(winsText)
+console.log(lossesInt)
 })
