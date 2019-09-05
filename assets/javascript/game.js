354a2eda6e0d86ab30600e6b62148b36f750f4f2
wheeters <wheetmansw@gmail.com>
2019-09-04 21:21:12 -0700

    finished functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //tutor help refrence for organizing data
     // var state = {
     //     elements: {
     //         lossesText: $()
@@ -6,13 +7,13 @@ $(document).ready(function () {
     //     wins: 0,
     //     losses
     // }
-var lossesText = $("#lose-text")
+var lossesText = $("#losses-text")
 var winsText = $("#wins-text");
-var crystals = []
+var crystals = ['','','','']
 var computerStartingNumber = $("#comp-score")
 var userTotalid = $("#user-total")
 var userTotal = 0
-var startingNumber;
+var startingNumber = 0;
 var winsInt = 0
 var lossesInt = 0
 var crystalElements = [ $("#crystal1"), $("#crystal2"), $("#crystal3"), $("#crystal4") ]
@@ -28,16 +29,16 @@ var randomNumberC = (Math.floor(Math.random()* 20)+1)
 return (randomNumberC)
 }
 for (var i = 0; i < crystals.length; i++) {
-     crystals.push(numbergenC())
+     crystals[i]=(numbergenC())
      crystalElements[i].attr("crystal-index", i);
 }   
 
 }
 
 fillCrystalArray()
-startingNumberGen()
-computerStartingNumber.text(startingNumberGen())
 startingNumber = startingNumberGen()
+computerStartingNumber.text(startingNumber)
+
 
 
 
@@ -46,34 +47,41 @@ startingNumber = startingNumberGen()
 var gem = $('.gem')
 
 gem.click(function(event) {
+    
     var index = parseInt($(this).attr("crystal-index"))
     userTotal += crystals[index]
     userTotalid.text(userTotal)
     winCon()
-
  })
 
 function winCon (){
-    if (userTotal===startingNumber){
+    if (userTotal === startingNumber){
         console.log("you won")
         winsInt++
         winsText.text(winsInt)
         reset()
+        
     }
     else if(userTotal > startingNumber){
         console.log('you lost')
-        lossesInt ++
+        lossesInt++
         lossesText.text(lossesInt)
         reset()
+        
     }
 }
 function reset(){
     fillCrystalArray()
-    startingNumberGen()
-    computerStartingNumber.text(startingNumberGen())
+    startingNumber = startingNumberGen()
+    computerStartingNumber.text(startingNumber)
+    userTotal = 0
+    userTotalid.text(0)
+   
+ 
 
 }
 
 console.log(winsText)
 console.log(lossesInt)
+console.log(crystals)
 })
